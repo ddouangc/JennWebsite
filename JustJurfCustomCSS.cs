88938328c0d3e7913210991f1b8c9a5662cff784
Donnie Douangchak <ddouangc@gmail.com>
2018-06-01 22:19:30 -0700

    Initial push of code

---
+++ JustJurfCustomCSS.cs
@@ -0,0 +1,239 @@
+/*
+ * Script Fonts
+*/
+
+@font-face {
+  font-family: 'Adelicia';
+  	src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af11e8b6d2a7352b8feeb42/1525751435714/Adelicia+Script+Clean.otf),
+      url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af121841ae6cf6e52cf22f3/1525752197785/Adelicia+Script+Clean.ttf),
+      url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af1218f2b6a284dd4289061/1525752207891/Adelicia+Script+Clean.woff),
+      url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af1219d1ae6cf6e52cf25a8/1525752221488/Adelicia+Script+Clean.woff2);
+  font-weight: normal;
+  font-style: normal;
+}
+.Adelicia {
+    font-family : 'Adelicia', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Amelia';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af13483758d46982d658253/1525757061156/AmeliaScript-FineVersion.otf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af134921ae6cf6e52d162c1/1525757075169/AmeliaScript-FineVersion.ttf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Amelia {
+    font-family : 'Amelia', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Anter';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af1357df950b790adcb003a/1525757310373/Anter.otf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af135818a922d778362e84b/1525757313223/Anter.ttf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Anter {
+    font-family : 'Anter', sans-serif;
+  }
+
+
+@font-face {
+  font-family: 'Argentinian';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af135cc2b6a284dd42aef5a/1525757388993/Argentinian+Nights.otf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af135cf6d2a7352b801a8c6/1525757391695/Argentinian+Nights+by+Kestrel+Montes.woff);
+  font-weight: normal;
+  font-style: normal;
+}
+.Argentinian {
+    font-family : 'Argentinian', sans-serif;
+  }
+
+
+@font-face {
+  font-family: 'Asterism';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af141856d2a7352b802be16/1525760391220/Asterism.otf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Asterism {
+    font-family : 'Asterism', sans-serif;
+}
+
+
+@font-face {
+  font-family: 'AtMahogany';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af141d503ce648f197a434b/1525760470067/atmahoganyscript.ttf);
+  font-weight: normal;
+  font-style: normal;
+}
+.AtMahogany {
+    font-family : 'AtMahogany', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Beloved';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af1426d575d1f8324cea7b6/1525760629527/Beloved+Script.ttf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af142078a922d7783640ce6/1525760526245/Beloved+Script.otf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Beloved {
+    font-family : 'Beloved', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Brunella';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af14369352f538d743049b5/1525760875177/Brunella+Script+Style.ttf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5af142078a922d7783640ce6/1525760526245/Beloved+Script.otf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Brunella {
+    font-family : 'Brunella', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Canvas';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f80d8352f5392c8a65263/1527742680866/Canvas+Pointed+Script+New+Update.ttf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f80dbaa4a99959a473ce3/1527742683647/Canvas+Pointed+Script+New+Update.otf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Canvas {
+    font-family : 'Canvas', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Carolyna';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f81180e2e7294c7cad39d/1527742745858/Emily+Lime+-+CarolynaPro.ttf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f811df950b73249edb19f/1527742750538/Emily+Lime+-+CarolynaPro.otf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Carolyna {
+    font-family : 'Daughter', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Daughter';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f815703ce6436bf009b24/1527742808688/Daughter+Script.otf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f815b70a6ad58c23d24b0/1527742813003/Daughter+Script.ttf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Daughter {
+    font-family : 'Canvas', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Madina';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f818288251b4adb418e3d/1527742852991/Madina+Script.ttf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f8191f950b73249edbeef/1527742865116/Madina+Ornaments.otf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Madina {
+    font-family : 'Madina', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Maphylla';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f81ad1ae6cfea0d9257a1/1527742894300/Maphylla.ttf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f81b06d2a73dba0d8cb9f/1527742896866/Maphylla.otf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Maphylla {
+    font-family : 'Maphylla', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Melika';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f81f50e2e7294c7caec90/1527742966266/melika_letter-webfont.ttf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f81fc2b6a282944e4c773/1527742972146/melika_letter-webfont.woff),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f81ff2b6a282944e4c7e0/1527742975622/melika_letter-webfont.woff2);
+  font-weight: normal;
+  font-style: normal;
+}
+.Melika {
+    font-family : 'Melika', sans-serif;
+  }
+
+@font-face {
+  font-family: 'MonteCarlo';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f822f6d2a73dba0d8d9b5/1527743023670/MonteCarloScriptC-Regular.otf);
+  font-weight: normal;
+  font-style: normal;
+}
+.MonteCarlo {
+    font-family : 'MonteCarlo', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Mozart';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f82572b6a282944e4d1e5/1527743065138/MozartScript-Regular.ttf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Mozart {
+    font-family : 'Mozart', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Quickpen';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f8285aa4a99959a476b44/1527743110646/Trial+by+Cupcakes+-+Quickpen.ttf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f8289f950b73249eddb5b/1527743113697/Trial+by+Cupcakes+-+Quickpen.otf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Quickpen {
+    font-family : 'Quickpen', sans-serif;
+  }
+
+
+/*
+ * Serif Sans Fonts
+ */
+@font-face {
+  font-family: 'CongenialThin';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f8324f950b73249edeca7/1527743269513/Congenial+Thin.ttf),
+    url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f8327aa4a99959a477e35/1527743271722/Congenial+Thin.otf);
+  font-weight: normal;
+  font-style: normal;
+}
+.CongenialThin {
+    font-family : 'CongenialThin', sans-serif;
+  }
+
+@font-face {
+  font-family: 'Geosans';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f8344f950b73249edeff2/1527743300229/GeosansLight.ttf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Geosans {
+    font-family : 'Geosans', sans-serif;
+  }
+
+
+@font-face {
+  font-family: 'Raleway';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f83911ae6cfea0d928ccc/1527743377705/Raleway-Regular.ttf);
+  font-weight: normal;
+  font-style: normal;
+}
+.Raleway {
+    font-family : 'Raleway', sans-serif;
+  }
+
+
+@font-face {
+  font-family: 'MrsEaves';
+  src : url(https://static1.squarespace.com/static/5a0ddc9a2278e7f9bddd9901/t/5b0f83db70a6ad58c23d6c08/1527743452320/Mrs+Eaves.otf);
+  font-weight: normal;
+  font-style: normal;
+}
+.MrsEaves {
+    font-family : 'MrsEaves', sans-serif;
+  }
